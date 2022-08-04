676c4cd1bbacd250584b55551d7bdb69674978bc
Mark Wylde <me@markwylde.com>
2022-08-04 21:20:47 +0100

    Resolve PR comments:

--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-function ReactAccordion(props) {
-    const { items } = props;
-    return (_jsx("ul", { children: items.map((item) => (_jsx("li", { children: _jsxs("details", { "aria-label": item.name, open: item.open, children: [_jsx("summary", { children: item.heading }), item.child] }) }, item.name))) }));
-}
-export default ReactAccordion;
-//# sourceMappingURL=index.js.map
