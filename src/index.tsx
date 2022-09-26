006d15e0e7dd2c3e7275288d2d171b38054f91ab
Mark Wylde <markwylde@users.noreply.github.com>
2022-09-26 19:38:41 +0100

    Support esm cjs and split demo app into own project (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -9,23 +9,22 @@ interface ReactAccordionProps {
   }[]
 }
 
-const ReactAccordion = (props: ReactAccordionProps) => {
+export function ReactAccordion(props: ReactAccordionProps) {
+  const { items } = props;
+
   return (
     <ul>
-      {props.items.map(item => {
-        
-        return (
-          <li key={item.name}>
-            <details aria-label={item.name} open={item.open}>
-              <summary>{item.heading}</summary>
-              {item.child}
-            </details>
-          </li>
-        );
-
-      })}
+      {items.map((item) => (
+        <li key={item.name}>
+          <details aria-label={item.name} open={item.open}>
+            <summary>{item.heading}</summary>
+            {item.child}
+          </details>
+        </li>
+      ))}
     </ul>
   );
 }
 
 export default ReactAccordion;
+module.exports = ReactAccordion;
